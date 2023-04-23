2ae0bd81faf97c5a2806363ad0e5fc13366b6f19
Gustave F. Gerhardt <gus@thewirecutter.com>
2023-04-23 11:31:57 -0500

    Add conversion for weather icons

--- tomorrow.js
+++ tomorrow.js
@@ -18,41 +18,58 @@ setGlobal("MoonPhase", day.intervals[0].values.moonPhase);
 // Humidity
 setGlobal("Humidity", Math.round(current.intervals[0].values.humidity));
 
+
+     'clear-day': 'day',
+      'clear-night': 'night',
+      'rain': 'rainy-5',
+      'snow': 'snowy-6',
+      'sleet': 'rainy-6',
+      'wind': 'cloudy',
+      'fog': 'cloudy',
+      'cloudy': 'cloudy',
+      'partly-cloudy-day': 'cloudy-day-3',
+      'partly-cloudy-night': 'cloudy-night-3',
+      'hail': 'rainy-7',
+      'lightning': 'thunder',
+      'thunderstorm': 'thunder',
+      'windy-variant': html`cloudy-${this.dayOrNight}-3`,
+      'exceptional': '!!',
+
 var weatherCodesObj = {
-  0: "Unknown",
-  1000: "Clear, Sunny",
-  1100: "Mostly Clear",
-  1101: "Partly Cloudy",
-  1102: "Mostly Cloudy",
-  1001: "Cloudy",
-  2000: "Fog",
-  2100: "Light Fog",
-  4000: "Drizzle",
-  4001: "Rain",
-  4200: "Light Rain",
-  4201: "Heavy Rain",
-  5000: "Snow",
-  5001: "Flurries",
-  5100: "Light Snow",
-  5101: "Heavy Snow",
-  6000: "Freezing Drizzle",
-  6001: "Freezing Rain",
-  6200: "Light Freezing Rain",
-  6201: "Heavy Freezing Rain",
-  7000: "Ice Pellets",
-  7101: "Heavy Ice Pellets",
-  7102: "Light Ice Pellets",
-  8000: "Thunderstorm"
+  0: { text: "Unknown", code: "unknown" },
+  1000: { text: "Clear, Sunny", code: "clear-day" },
+  1100: { text: "Mostly Clear", code: "clear-day" },
+  1101: { text: "Partly Cloudy", code: "partly-cloudy-day" },
+  1102: { text: "Mostly Cloudy", code: "cloudy" },
+  1001: { text: "Cloudy", code: "cloudy" },
+  2000: { text: "Fog", code: "fog" },
+  2100: { text: "Light Fog", code: "fog" },
+  4000: { text: "Drizzle", code: "rain" },
+  4001: { text: "Rain", code: "rain" },
+  4200: { text: "Light Rain", code: "rain" },
+  4201: { text: "Heavy Rain", code: "rain" },
+  5000: { text: "Snow", code: "snow" },
+  5001: { text: "Flurries", code: "snow" },
+  5100: { text: "Light Snow", code: "snow" },
+  5101: { text: "Heavy Snow", code: "snow" },
+  6000: { text: "Freezing Drizzle", code: "sleet" },
+  6001: { text: "Freezing Rain", code: "sleet" },
+  6200: { text: "Light Freezing Rain", code: "sleet" },
+  6201: { text: "Heavy Freezing Rain", code: "sleet" },
+  7000: { text: "Ice Pellets", code: "sleet" },
+  7101: { text: "Heavy Ice Pellets", code: "sleet" },
+  7102: { text: "Light Ice Pellets", code: "sleet" },
+  8000: { text: "Thunderstorm", code: "rain" }
 };
 
 // Summary
-setGlobal("WeatherSummary", weatherCodesObj[current.intervals[0].values.weatherCode]);
+setGlobal("WeatherSummary", weatherCodesObj[current.intervals[0].values.weatherCode].text);
 
 // Icons
-setGlobal("WeatherIcon", current.intervals[0].values.weatherCode);
-setGlobal("WeatherIconNext", hourly.intervals[2].values.weatherCode);
-setGlobal("WeatherIconNext2", hourly.intervals[4].values.weatherCode);
-setGlobal("WeatherIconNext3", hourly.intervals[6].values.weatherCode);
+setGlobal("WeatherIcon", weatherCodesObj[current.intervals[0].values.weatherCode].code);
+setGlobal("WeatherIconNext", weatherCodesObj[hourly.intervals[2].values.weatherCode].code);
+setGlobal("WeatherIconNext2", weatherCodesObj[hourly.intervals[4].values.weatherCode].code);
+setGlobal("WeatherIconNext3", weatherCodesObj[hourly.intervals[6].values.weatherCode].code);
 
 // Precipitation
 setGlobal("Precipitation", current.intervals[0].values.precipitationProbability);
